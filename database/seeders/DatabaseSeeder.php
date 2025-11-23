a1287d5b311a2b2655b1e0d4f21108e6c7e20b86
SvetlanaNesmiyan <Svitlana.Nesmiian@infiz.khpi.edu.ua>
2025-11-23 21:38:24 +0200

    laravel-setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,30 +12,35 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        // Очищаємо таблиці
+        \DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Comment::truncate();
+        Task::truncate();
+        \DB::table('project_user')->truncate();
+        Project::truncate();
+        User::truncate();
+        \DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
         // Створюємо 8 користувачів
         $users = User::factory(8)->create();
-
-        // Додаємо тестового користувача
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
 
         // Створюємо 5 проектів
         $projects = Project::factory(5)->create();
 
-        // Додаємо користувачів до проектів (зв'язок many-to-many)
+        // Додаємо користувачів до проектів (3-4 користувачі на проект)
         foreach ($projects as $project) {
-            $project->users()->attach(
-                $users->random(3)->pluck('id'),
-                ['role' => 'member']
-            );
+            $projectUsers = $users->random(rand(3, 4));
+            foreach ($projectUsers as $user) {
+                $project->users()->attach($user->id, [
+                    'role' => $user->id === $project->owner_id ? 'owner' : 'member'
+                ]);
+            }
         }
 
-        // Створюємо 20 задач
-        $tasks = Task::factory(20)->create();
+        // Створюємо 8 задач
+        $tasks = Task::factory(8)->create();
 
-        // Створюємо 60 коментарів (по 3 на задачу)
-        Comment::factory(60)->create();
+        // Створюємо 5-8 коментарів
+        Comment::factory(rand(5, 8))->create();
     }
 }
